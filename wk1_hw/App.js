8dfd614cc49429c1cf8932c0328db4eb2c83ca7e
dddCrazy87 <n126970@gmail.com>
2024-02-20 22:45:04 +0800

    create wk1_hw app

---
+++ wk1_hw/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>111119020 許昀韋</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
