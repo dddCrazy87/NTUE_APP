504714f89b8434c6bbb3666574222ac98b473501
dddCrazy87 <n126970@gmail.com>
2024-03-20 01:17:17 +0800

    add navigation bar

--- wk3_hw/wk3_hw/ContentView.swift
+++ wk3_hw/wk3_hw/ContentView.swift
@@ -1,15 +1,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    var body: some View {
-        VStack {
-            Text("Popular Books")
-                .font(.title)
-                .padding()
 
-            BookView(books: popular_books)
+    var body: some View {
+        
+        NavigationView {
+            VStack {
+                
+                Text("Popular Books")
+                    .font(.title)
+                    .padding()
+
+                BookView(books: popular_books)
+            }
+            .navigationTitle("")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "line.horizontal.3")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "magnifyingglass")
+                    }
+                }
+            }
         }
+        
+        
+        
+        
     }
 }
 
