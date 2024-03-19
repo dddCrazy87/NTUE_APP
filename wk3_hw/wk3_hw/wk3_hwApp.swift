a3f53daf0388f265c876189d87ad1e2df49baa50
dddCrazy87 <n126970@gmail.com>
2024-03-20 00:08:04 +0800

    create project

---
+++ wk3_hw/wk3_hw/wk3_hwApp.swift
@@ -0,0 +1,17 @@
+//
+//  wk3_hwApp.swift
+//  wk3_hw
+//
+//  Created by 許昀韋 on 2024/3/20.
+//
+
+import SwiftUI
+
+@main
+struct wk3_hwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
