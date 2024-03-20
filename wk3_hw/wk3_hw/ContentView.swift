43b0204526c779f66fb6369d7f53b323d554ee3d
dddCrazy87 <n126970@gmail.com>
2024-03-20 11:26:54 +0800

    modify bottom bar style

--- wk3_hw/wk3_hw/ContentView.swift
+++ wk3_hw/wk3_hw/ContentView.swift
@@ -29,9 +29,51 @@ struct ContentView: View {
                 
                 VStack {
                     Spacer()
-                    Image("bottom_bar")
-                        .resizable()
-                        .frame(width: 360, height: 56)
+                    HStack {
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image("home")
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                                Text("Home")
+                                    .font(.system(size: 12))
+                                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            }
+                        }
+                        .padding()
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image("wishlist")
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                                Text("Wishlist")
+                                    .font(.system(size: 12))
+                                    .foregroundColor(Color(red: 0.4, green: 0.4, blue: 0.4))
+                            }
+                        }
+                        .padding()
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image("mybooks")
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                                Text("My books")
+                                    .font(.system(size: 12))
+                                    .foregroundColor(Color(red: 0.4, green: 0.4, blue: 0.4))
+                            }
+                        }
+                        .padding()
+                    }
+                    .frame(width: 360, height: 56)
+                    .background(Color.white)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                    .shadow(color: Color.black.opacity(0.3), radius: 3, x: 0, y: -5)
                 }
                 
                 if isMenuOpen {
